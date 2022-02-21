555de0279aa171294b8cb9ff3d346ef4ace94873
jamesfill <jamesp.fillerup@gmail.com>
2022-02-21 15:06:15 -0700

    date colors and editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,11 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
+
   var taskP = $("<p>")
     .addClass("m-1")
     .text(taskText);
@@ -13,6 +15,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -207,17 +211,25 @@ $(".list-group").on("click", "span", function(){
 
     //swap out elements
     $(this).replaceWith(dateInput);
-
+    
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate:1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event to the dateInput
+      $(this).trigger("change");
+    }
+  });
     //automatically focus on new elements
   dateInput.trigger("focus");
 });
 
 //value of the due date was changed
-$(".list-group").on("blur", "input[type ='text']", function(){
+$(".list-group").on("change", "input[type ='text']", function(){ // by having this be a change and not a blur it 
+
   //get current text
   var date=$(this)
-    .val()
-    .trim();
+    .val();
 
   //get the parent ul's id attribute
   var status =$(this)
@@ -242,8 +254,37 @@ $(".list-group").on("blur", "input[type ='text']", function(){
   //replace input with span element
   $(this).replaceWith(taskSpan);
 
+  //pass tasks <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
+var auditTask = function(taskEl) {
+  //get date from task element
+  var date = $(taskEl)
+  .find("span")
+  .text()
+  .trim();
+  //ensure it worked
+  console.log(date);
+
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // this should print out and object for the value of the date vaiable
+  console.log(time);
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new clas if task is near/over due date
+  if (moment().isAfter(time)){
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days"))<= 2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
+$("#modalDueDate").datepicker({ // this line brings up the calendar grid
+  minDate:1 //this makes it so that you cant select dates that have passed
+});
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
